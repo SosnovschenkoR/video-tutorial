ca671f409393344357de8adb3eb3d84add081fd6
SosnovschenkoR <memphis@com.mels.ru>
2016-07-31 00:43:47 +0300

    test commint

--- src/com/MainApp.java
+++ src/com/MainApp.java
@@ -2,6 +2,6 @@ package com;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Running app...");
+        System.out.println("Running  app...");
     }
 }
